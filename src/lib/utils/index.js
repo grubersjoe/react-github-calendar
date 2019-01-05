cab6b26e02b9888e5d6818192241edcf6e57b17f
Jonathan Gruber <gruberjonathan@gmail.com>
2019-01-05 23:32:09 +0100

    Add parameter to control empty cell background color

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -6,11 +6,17 @@ import { DEFAULT_THEME } from './constants';
  * Create a calendar color theme based on one color
  * @param {string} baseColor - The base color all lighter hues are calculated from
  * @param {string} textColor - Text color
+ * @param {string} emptyCellColor
  * @param {string} background - Calendar background color. `transparent` by default
  * @throws {Error} - Throws an error if the color could not be parsed
  * @return {object} - The color theme object
  */
-export function createCalendarTheme(baseColor, textColor = 'inherit', background = 'transparent') {
+export function createCalendarTheme(
+  baseColor,
+  textColor = 'inherit',
+  emptyCellColor = color('white').darken(8).toHslString(),
+  background = 'transparent'
+) {
   const base = color(baseColor);
 
   if (!base.isValid()) {
@@ -26,6 +32,6 @@ export function createCalendarTheme(baseColor, textColor = 'inherit', background
     grade3: base.setAlpha(0.76).toHslString(),
     grade2: base.setAlpha(0.60).toHslString(),
     grade1: base.setAlpha(0.44).toHslString(),
-    grade0: color('white').darken(8).toHslString(),
+    grade0: emptyCellColor,
   };
 }
