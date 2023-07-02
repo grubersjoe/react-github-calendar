89c7b3c5330cf3be336c590bb3dc7162d78ae1d1
Jonathan Gruber <gruberjonathan@gmail.com>
2023-07-02 12:01:07 +0200

    Set base URL

--- example/vite.config.js
+++ example/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 // @see https://vitejs.dev/guide/dep-pre-bundling.html
 export default defineConfig({
   plugins: [react()],
+  // eslint-disable-next-line no-undef
+  base: process.env.NODE_ENV === 'production' ? '/react-github-calendar/' : undefined,
   server: {
     port: 8080,
   },
