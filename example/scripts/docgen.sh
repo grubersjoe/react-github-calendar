edbcdd19357e061c85811ce30ce2bd4c78fe83ca
Jonathan Gruber <gruberjonathan@gmail.com>
2026-05-21 22:21:01 +0200

    Move script

--- example/scripts/docgen.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-dirname=$(dirname "$(realpath "$0")")
-cd "$dirname/../../node_modules/react-activity-calendar/src/components" || exit 1
-json=$(pnpm dlx @react-docgen/cli ActivityCalendar.tsx)
-echo "$json" | jq '.["ActivityCalendar.tsx"][0]' > "$dirname/../src/docgen.json"
